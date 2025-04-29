d227f62aa0118c7305cc4a230a387001d1344627
Joshua "Shadow" Monson <4204089+ShadowLordAlpha@users.noreply.github.com>
2025-04-29 08:55:07 -0700

    chore: Upgrade SC2 Proto Version
    is this needed? no its literally just a version update because blizzard doesn't do anything and this is automated but we should be using the latest anyway

--- thirdparty/cmake/sc2protocol.cmake
+++ thirdparty/cmake/sc2protocol.cmake
@@ -3,6 +3,6 @@ message(STATUS "FetchContent: protocol")
 FetchContent_Declare(
     sc2protocol
     GIT_REPOSITORY https://github.com/Blizzard/s2client-proto.git
-    GIT_TAG c04df4adbe274858a4eb8417175ee32ad02fd609
+    GIT_TAG a6d60f4cce7886a3be2bf7af2c1b12fdab6e9c63
 )
 FetchContent_MakeAvailable(sc2protocol)
