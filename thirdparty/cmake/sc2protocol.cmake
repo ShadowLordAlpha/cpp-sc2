06c9c40580f6cda6bf6c110272ac942b7ce0d321
Joshua "Shadow" Monson <4204089+ShadowLordAlpha@users.noreply.github.com>
2025-05-03 23:20:38 -0700

    Update Proto CMake file

--- thirdparty/cmake/sc2protocol.cmake
+++
@@ -1,8 +0,0 @@
-message(STATUS "FetchContent: protocol")
-
-FetchContent_Declare(
-    sc2protocol
-    GIT_REPOSITORY https://github.com/Blizzard/s2client-proto.git
-    GIT_TAG a6d60f4cce7886a3be2bf7af2c1b12fdab6e9c63
-)
-FetchContent_MakeAvailable(sc2protocol)
